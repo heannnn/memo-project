6655cd50442f1380bc8b96e603947368398932ec
heannnn <jse033101@gmail.com>
2022-06-01 23:21:31 +0900

    첫번째 메모 수정

--- src/App.js
+++ src/App.js
@@ -9,20 +9,28 @@ function App() {
       title: 'Memo1',
       content: 'This is memo1',
       createdAt: 1654064360688, // 시간
-      updateAt: 1654064371505,
+      updatedAt: 1654064371505,
     },
     {
       title: 'Memo2',
       content: 'This is memo2',
       createdAt: 1654064386920, // 시간
-      updateAt: 1654064391128,
+      updatedAt: 1654064391128,
     },
   ]);
+
+  const [selectedMemoIndex, setSelectedMemoIndex] = useState(0);
+  const setMemo = (newMemo) => {
+    // memos[selectedMemoIndex] = newMemo; // 직접적으로 newMemo를 할당하므로 memos 를 바꿔버림. 불변성 훼손.
+    const newMemos = [...memos]; // 미리 memos copy후에 값 할당. 불변성 유지.
+    newMemos[selectedMemoIndex] = newMemo;
+    setMemos([...memos]); // 리렌더링을 위해 새로운 reference 설정
+  };
 
   return (
     <div className="App">
       <SideBar memos={memos} />
-      <MemoContainer />
+      <MemoContainer memo={memos[selectedMemoIndex]} setMemo={setMemo} />
     </div>
   );
 }
